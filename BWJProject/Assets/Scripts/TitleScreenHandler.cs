5d6a3e84fc2b5d04a4b1c03a98a7f628ef02f59e
KendeMarcellPinke <pkm.gamedev@gmail.com>
2022-04-02 18:26:50 +0200

    lil 3d work

--- BWJProject/Assets/Scripts/TitleScreenHandler.cs
+++ BWJProject/Assets/Scripts/TitleScreenHandler.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TitleScreenHandler : MonoBehaviour
 {
+    public float blinkTime = 0.4F;
+    public GameObject textTMP;
+    bool run = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(TextBlink());
     }
 
     // Update is called once per frame
@@ -18,6 +22,22 @@ public class TitleScreenHandler : MonoBehaviour
         {
             SceneManager.LoadScene(1);
         }
-
+        
+    }
+    private IEnumerator TextBlink()
+    {
+        while (run)
+        {
+            if (textTMP.activeInHierarchy)
+            {
+                textTMP.SetActive(true);
+            }
+            else
+            {
+                textTMP.SetActive(false);
+            }
+            yield return new WaitForSeconds(blinkTime);
+        }
+        yield break;
     }
 }
